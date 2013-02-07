e552c52f737d7bc8741480478dd7b64daefd9cf6
vaaslee <lee.zigshin@gmail.com>
2013-02-07 18:35:21 +0500

    This script encrypts the word through the Caesar cipher

---
+++ caesar.py
@@ -0,0 +1,34 @@
+#This function will take the character and the key
+#s = character; k = key;
+def tocrypte(s,k):
+	#alphabet = list of English letters
+	alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
+	#ind = index of character
+	ind = alphabet.index(s) + k
+	#letter = returning letter
+	if ind > 25:
+		ind = ind - 25 -1
+	letter = alphabet[ind]
+	return letter
+#
+#
+#
+
+#word = our word
+word = raw_input("Type the word you want to encrypt(enter /exit to close the program):")
+key = input("Type the key:")
+i = 0
+while word != "/exit":
+	result = []
+	for x in word:
+		result.append(tocrypte(word[i], key))
+		i = i + 1
+	result = ''.join(result)
+	print result
+	word = raw_input("Type the word you want to encrypt(enter /exit to close the program):")
+	if word != "/exit":
+		i = 0
+		key = input("Type the key:")
+
+		 
+
