a4cf438d812f9978ebf1dd6a54dd3d0341ff1e94
vaaslee <lee.zigshin@gmail.com>
2013-02-09 16:31:14 +0500

    caesar.py has been modified.

--- caesar.py
+++ caesar.py
@@ -2,6 +2,8 @@
 #s = character; k = key;
 def tocrypte(s,k):
 	#alphabet = list of English letters
+	if s == " ":
+		return " "
 	alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
 	#ind = index of character
 	ind = alphabet.index(s) + k
